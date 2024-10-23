f3817cc72f4e8ddbf764e1708a6672ee85fb0f74
LimbergDV <limbergdelvalle@gmail.com>
2024-10-23 06:56:17 -0600

    login started

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FilePageComponent } from './files/file-page/file-page.component';
+import { LoginPageComponent } from './login/login-page/login-page.component';
 
 const routes: Routes = [
-  {path:'uploadFiles', component: FilePageComponent }
+  {path:'uploadFiles', component: FilePageComponent },
+  {path:'login', component: LoginPageComponent}
 ];
 
 @NgModule({
