bc4ba133ee6d59f0bfe280e142eba77cba650555
LimbergDV <limbergdelvalle@gmail.com>
2024-10-23 16:15:50 -0600

    add: editing app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { IActionHome } from './interfaces/iaction-home';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +6,4 @@ import { IActionHome } from './interfaces/iaction-home';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  
 }
