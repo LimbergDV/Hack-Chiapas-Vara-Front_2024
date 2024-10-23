40d086b483c74b4244a376617df8332f27f986c5
Andresito126 <creed126ax@gmail.com>
2024-10-23 15:02:15 -0600

    fixing nav and aside

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FilePageComponent } from './files/file-page/file-page.component';
 import { LoginPageComponent } from './login/login-page/login-page.component';
+import { FoldersFilesPageComponent } from './folders-files/folders-files-page/folders-files-page.component';
 
 const routes: Routes = [
-  {path:'uploadFiles', component: FilePageComponent },
-  {path:'login', component: LoginPageComponent}
+  {path:'folders', component: FoldersFilesPageComponent },
+  {path:'login', component: LoginPageComponent},
+
+
+  
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { IActionHome } from './interfaces/iaction-home';
 
 @Component({
   selector: 'app-root',
