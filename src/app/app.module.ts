2aabb01b67e13a42b225236444e32c931f5a1575
Andresito126 <creed126ax@gmail.com>
2024-10-23 07:15:24 -0600

    Adding anomations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeLandingModule } from './home-landing/home-landing.module';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
+    NavBarComponent
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    HomeLandingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home-landing/home-landing.module.ts
+++ src/app/home-landing/home-landing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeLandingPageComponent } from './home-landing-page/home-landing-page.component';
+import { AnimationSliderComponent } from './animation-slider/animation-slider.component';
+
+
 
 
 
 @NgModule({
   declarations: [
-    HomeLandingPageComponent
+    HomeLandingPageComponent,
+    AnimationSliderComponent
   ],
   imports: [
     CommonModule
