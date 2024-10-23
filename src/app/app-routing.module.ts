0fdfcadafded5650c5e10fa333e1335370a080b7
Andresito126 <creed126ax@gmail.com>
2024-10-23 05:59:52 -0600

    Add: starting the logo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FilePageComponent } from './files/file-page/file-page.component';
+import { HomeLandingPageComponent } from './home-landing/home-landing-page/home-landing-page.component';
 
 const routes: Routes = [
+  {path:'home-landing', component: HomeLandingPageComponent },
   {path:'uploadFiles', component: FilePageComponent }
 ];
 
