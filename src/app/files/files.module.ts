cd1a8a557d71778f5e050a839033418f2fae46fa
LimbergDV <limbergdelvalle@gmail.com>
2024-10-22 22:27:28 -0600

    add: editing a choose file in upload file

---
+++ src/app/files/files.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FilePageComponent } from './file-page/file-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    FilePageComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+})
+export class FilesModule { }
